d050ef96bbf2a05115f21443411bccad8e5ed48e
mehran <me_pro_comp@ymail.com>
2018-09-04 18:44:57 +0430

    fix error

---
+++ tmt/example-perplexity.scala
@@ -0,0 +1,56 @@
+// Stanford TMT Example 5 - Selecting LDA model parameters
+// http://nlp.stanford.edu/software/tmt/0.4/
+
+// tells Scala where to find the TMT classes
+import scalanlp.io._;
+import scalanlp.stage._;
+import scalanlp.stage.text._;
+import scalanlp.text.tokenize._;
+import scalanlp.pipes.Pipes.global._;
+import java.io._;
+import edu.stanford.nlp.tmt.stage._;
+import edu.stanford.nlp.tmt.model.lda._;
+import edu.stanford.nlp.tmt.model.llda._;
+
+val source = CSVFile("dataset.csv") ~> IDColumn(1);
+
+val tokenizer = {
+  SimpleEnglishTokenizer() ~>            // tokenize on space and punctuation
+  //CaseFolder() ~>                        // lowercase everything
+  //WordsAndNumbersOnlyFilter() ~>         // ignore non-words and non-numbers
+  MinimumLengthFilter(1)                 // take terms with >=3 characters
+}
+
+val text = {
+  source ~>                              // read from the source file
+  Column(2) ~>                           // select column containing text
+  TokenizeWith(tokenizer) ~>             // tokenize with tokenizer above
+  TermCounter() ~>                       // collect counts (needed below)
+  TermMinimumDocumentCountFilter(1) ~>   // filter terms in <4 docs
+  TermDynamicStopListFilter(0) ~>       // filter out 30 most common terms
+  DocumentMinimumLengthFilter(1)         // take only docs with >=5 terms
+}
+// build a training dataset
+val training = LDADataset(text);
+
+// a list of pairs of (number of topics, perplexity)
+var scores = List.empty[(Int,Double)];
+
+// loop over various numbers of topics, training and evaluating each model
+for (numTopics <- List(100,1000,2000,3000,5000,7000,9000,10000,20000,30000,40000)) {
+  val params = LDAModelParams(numTopics = numTopics, dataset = training);
+  val output = file("lda-"+training.signature+"-"+params.signature);
+  val model = TrainCVB0LDA(params, training, output=null, maxIterations=500);
+  
+  val perplexity = model.computePerplexity(training);
+
+  scores :+= (numTopics, perplexity);
+}
+
+
+val pw = new PrintWriter(new File("res.txt" ));
+for ((numTopics,perplexity) <- scores) {
+  pw.write("[perplexity] perplexity at "+numTopics+" topics: "+perplexity+"\n");
+}
+pw.close();
+
